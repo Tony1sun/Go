2676766fb76ee670def7894f646f084c4356ae23
‘jack’ <jacktony822@gmail.com>
2021-06-07 23:35:35 +0800

    commit

--- src/code.oldboyedu.com/studygo/day03/05defer_return/main.go
+++ src/code.oldboyedu.com/studygo/day03/05defer_return/main.go
@@ -13,7 +13,7 @@ func f1() int {
 	defer func() {
 		x++ //修改的是x，不是返回值
 	}()
-	return x
+	return x // 返回值赋值  2. defer  3.真正的RET指令
 }
 
 func f2() (x int) {
@@ -31,8 +31,25 @@ func f3() (y int) {
 	return x
 }
 
+func f4() (x int) {
+	defer func(x int) {
+		x++
+	}(x)
+	return 5 // 返回值  = x = 5
+}
+
+func f5() (x int) {
+	defer func(x int) int {
+		x++ // 改变的是函数中的x的副本
+		return x
+	}(x)
+	return 5
+
+}
 func main() {
-	fmt.Println(f1())
-	fmt.Println(f2())
-	fmt.Println(f3())
+	fmt.Println(f1()) // 5
+	fmt.Println(f2()) // 6
+	fmt.Println(f3()) // 5
+	fmt.Println(f4()) // 5
+	fmt.Println(f5()) // 5
 }
