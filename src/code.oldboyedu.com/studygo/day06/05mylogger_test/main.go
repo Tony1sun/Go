51d985ad70c3021a27fffc9624c96527c88f7193
jacktony <jacktony822@gmail.com>
2021-06-18 19:48:19 +0800

    -m

--- src/code.oldboyedu.com/studygo/day06/05mylogger_test/main.go
+++ src/code.oldboyedu.com/studygo/day06/05mylogger_test/main.go
@@ -1,13 +1,21 @@
 package main
 
-import "code.oldboyedu.com/studygo/day06/mylogger"
+import (
+	"code.oldboyedu.com/studygo/day06/mylogger"
+	"time"
+)
 
 // 测试写的日志库
 func main() {
-	log := mylogger.NewLog()
+	log := mylogger.NewLog("info")
 	for {
 		log.Debug("Debug测试日志")
+		log.Trace("trace测试日志")
 		log.Info("Info测试日志")
+		log.Warning("warning测试日志")
+		log.Error("error测试日志")
+		log.Fatal("fatal测试日志")
+		time.Sleep(time.Second * 3)
 	}
 
 }
--- src/code.oldboyedu.com/studygo/day06/mylogger/console.go
+++ src/code.oldboyedu.com/studygo/day06/mylogger/console.go
@@ -1,36 +1,70 @@
 package mylogger
 
-import "fmt"
-
-// 在终端写日志相关内容
+import (
+	"fmt"
+	"time"
+)
 
 // Logger 日志结构体
 type Logger struct {
-
+	Level LogLevel
 }
 
 // Newlog 构造函数
-func NewLog() Logger {
-	return Logger{}
+func NewLog(levelStr string) Logger {
+	level, err := parseLogLevel(levelStr)
+	if err != nil {
+		panic(err)
+	}
+	return Logger{
+		Level: level,
+	}
 
 }
 
+func (l Logger) enable(logLevel LogLevel) bool {
+	return l.Level <= logLevel
+
+}
+
+func log(lv LogLevel, msg string) {
+	now := time.Now()
+	funcName, fileName, lineNo := getInfo(3)
+	fmt.Printf("[%s] [%s] [%s:%s:%d] %s\n", now.Format("2006-01-02 15:04:05"), getLogString(lv), fileName, funcName, lineNo, msg)
+
+}
 func (l Logger) Debug(msg string) {
-	fmt.Println(msg)
+	if l.enable(DEBUG) {
+		log(DEBUG, msg)
+	}
+}
+
+func (l Logger) Trace(msg string) {
+	if l.enable(TRACE) {
+		log(TRACE, msg)
+	}
 }
 
 func (l Logger) Info(msg string) {
-	fmt.Println(msg)
+	if l.enable(INFO) {
+		log(INFO, msg)
+	}
 }
 
 func (l Logger) Warning(msg string) {
-	fmt.Println(msg)
+	if l.enable(WARNING) {
+		log(WARNING, msg)
+	}
 }
 
 func (l Logger) Error(msg string) {
-	fmt.Println(msg)
+	if l.enable(ERROR) {
+		log(ERROR, msg)
+	}
 }
 
 func (l Logger) Fatal(msg string) {
-	fmt.Println(msg)
+	if l.enable(FATAL) {
+		log(FATAL, msg)
+	}
 }
