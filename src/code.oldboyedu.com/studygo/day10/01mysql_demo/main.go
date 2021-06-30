bcc9b1655936dfe57c568ab27ebdd7f4c0e51a37
jacktony <jacktony822@gmail.com>
2021-06-30 19:14:35 +0800

    提交

--- src/code.oldboyedu.com/studygo/day10/01mysql_demo/main.go
+++ src/code.oldboyedu.com/studygo/day10/01mysql_demo/main.go
@@ -12,11 +12,15 @@ func main() {
 	// 数据库信息
 	dsn := "root:123456@tcp(127.0.0.1:3306)/goday10"
 	// 连数据库
-	db, err := sql.Open("mysql", dsn)
+	db, err := sql.Open("mysql", dsn) // 不会校验用户名和密码是否正确
+	if err != nil {
+		fmt.Printf("dsn：%s invalid, err:%v\n", dsn, err)
+		return
+	}
+	err = db.Ping() // 尝试连接数据库
 	if err != nil {
 		fmt.Printf("open %s failed, err:%v\n", dsn, err)
 		return
 	}
-	db.Ping()
 	fmt.Println("连接数据库成功!")
 }
