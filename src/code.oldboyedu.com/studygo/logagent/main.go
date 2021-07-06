ca9190f9caeb8cb7faed22707afb4aa14a40b83f
‘jack’ <jacktony822@gmail.com>
2021-07-06 23:08:46 +0800

    add

--- src/code.oldboyedu.com/studygo/logagent/main.go
+++ src/code.oldboyedu.com/studygo/logagent/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"code.oldboyedu.com/studygo/logagent/conf"
 	"code.oldboyedu.com/studygo/logagent/kafka"
 	"code.oldboyedu.com/studygo/logagent/taillog"
 	"fmt"
@@ -9,6 +10,9 @@ import (
 )
 
 // LogAgent入口程序
+var (
+	cfg = new(conf.AppConf)
+)
 
 func run() {
 	// 1.读取日志
@@ -16,7 +20,7 @@ func run() {
 		select {
 		case line := <-taillog.ReadChan():
 			// 2.发送到kafka
-			kafka.SendToKafka("web_log", line.Text)
+			kafka.SendToKafka(cfg.KafkaConf.Topic, line.Text)
 		default:
 			time.Sleep(time.Second)
 		}
@@ -25,19 +29,25 @@ func run() {
 
 func main() {
 	// 0.加载配置文件
-	cfg, err := ini.Load("./conf/config.ini")
-	fmt.Println(cfg.Section("kafka").Key("address"))
-	fmt.Println(cfg.Section("kafka").Key("topic"))
-	fmt.Println(cfg.Section("taillog").Key("path"))
+	//cfg, err := ini.Load("./conf/config.ini")
+	//fmt.Println(cfg.Section("kafka").Key("address"))
+	//fmt.Println(cfg.Section("kafka").Key("topic"))
+	//fmt.Println(cfg.Section("taillog").Key("path"))
+
+	err := ini.MapTo(cfg, "./conf/config.ini")
+	if err != nil {
+		fmt.Println("load ini failed, err:%v\n", err)
+		return
+	}
 	// 1.初始化kafka连接
-	err = kafka.Init([]string{"127.0.0.1:9092"})
+	err = kafka.Init([]string{cfg.KafkaConf.Address})
 	if err != nil {
 		fmt.Printf("init kafka failed, err:%v\n", err)
 		return
 	}
 	fmt.Println("init kafka success")
 	// 2.打开日志文件准备收集日志
-	err = taillog.Init("./my.log")
+	err = taillog.Init(cfg.TaillogConf.FileName)
 	if err != nil {
 		fmt.Printf("init tailog failed, err:%v\n", err)
 		return
