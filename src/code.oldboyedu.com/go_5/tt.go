031e6628ceb671a32c429431fd04470f458bfa45
‘jack’ <jacktony822@gmail.com>
2021-07-13 23:14:15 +0800

    add

---
+++ src/code.oldboyedu.com/go_5/tt.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// gin的helloWorld
+
+func main() {
+	// 1.创建路由
+	r := gin.Default()
+	// 2.绑定路由规则
+	// gin.Context，封装了request和response
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello World!")
+	})
+	// 3.监听端口，默认再8080
+	r.Run(":8000")
+}
