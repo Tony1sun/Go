7fd70de62ef432b027c3a2c414a0793aea6ac169
‘jack’ <jacktony822@gmail.com>
2021-07-14 22:35:47 +0800

    add

--- src/code.oldboyedu.com/go_5/tt.go
+++ src/code.oldboyedu.com/go_5/tt.go
@@ -5,17 +5,39 @@ import (
 	"net/http"
 )
 
-// gin的helloWorld
+// 定义接受数据的结构体
+type Login struct {
+	// binding:"required" 修饰的字段，若接受为空值，则报错
+	User     string `form:"username" json:"user" uri:"user" xml:"user" binding:"required"`
+	Password string `form:"password" json:"password" uri:"password" xml:"password" binding:"required"`
+}
 
 func main() {
 	// 1.创建路由
 	r := gin.Default()
-	// 2.绑定路由规则
-	// gin.Context，封装了request和response
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello World!")
+	// JSON绑定
+	r.GET("/:user/:password", func(c *gin.Context) {
+		// 声明接收的变量
+		var login Login
+		// Bind()默认解析并绑定form格式
+		// 根据请求头中content-type自动推断
+		if err := c.ShouldBindUri(&login); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// JSON绑定
+		// 将request的body种的数据，自动按照json格式解析到结构体
+		//if err := c.ShouldBindJSON(&json); err != nil {
+		//	c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+		//	return
+		//}
+		// 判断用户名密码是否正确
+		if login.User != "root" || login.Password != "admin" {
+			c.JSON(http.StatusBadRequest, gin.H{"status": "304"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"status": "200"})
 	})
-	r.POST()
-	// 3.监听端口，默认再8080
 	r.Run(":8000")
 }
