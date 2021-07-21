ca48af9da08cac0dbac3cb7ed25d547188bc8a7b
‘jack’ <jacktony822@gmail.com>
2021-07-21 22:41:35 +0800

    add

--- src/code.oldboyedu.com/go_5_blog/service/article.go
+++ src/code.oldboyedu.com/go_5_blog/service/article.go
@@ -6,14 +6,55 @@ import (
 )
 
 // 获取文章和对应的分类
-func GetArticleRecordList(pageNum, pageSize int) (articleRecordList []model.ArticleRecord, err error) {
+func GetArticleRecordList(pageNum, pageSize int) (articleRecordList []*model.ArticleRecord, err error) {
 	// 获取文章列表
-	articleInfolist, err := db.GetAricleList(pageNum, pageSize)
+	articleInfoList, err := db.GetAricleList(pageNum, pageSize)
 	if err != nil {
 		return
 	}
-	if len(articleInfolist) <= 0 {
+	if len(articleInfoList) <= 0 {
 		return
 	}
 	// 获取文章对应的分类(多个)
+	cateforyIds := getCategoryIds(articleInfoList)
+	categoryList, err := db.GetCategoryList(cateforyIds)
+	if err != nil {
+		return
+	}
+	// 返回页面，做聚合
+	// 遍历所有文章
+	for _, article := range articleInfoList {
+		// 根据当前文章，生成结构体
+		articleRecord := &model.ArticleRecord{
+			ArticleInfo: *article,
+		}
+		// 文章取出分类id
+		categoryId := article.CategoryId
+		// 遍历分类列表
+		for _, category := range categoryList {
+			if categoryId == category.CategoryId {
+				articleRecord.Category = *category
+				break
+			}
+		}
+		articleRecordList = append(articleRecordList, articleRecord)
+	}
+	return
+}
+
+// 根据多个文章的id,获取多个分类id的集合
+func getCategoryIds(articleInfoList []*model.ArticleInfo) (ids []int64) {
+	// 遍历文章，得到每个文章
+	for _, article := range articleInfoList {
+		// 从当前文章取出分类id
+		categoryId := article.CategoryId
+		// 去重相同的分类id
+		for _, id := range ids {
+			// 看当前id是否存在
+			if id != categoryId {
+				ids = append(ids, categoryId)
+			}
+		}
+	}
+	return
 }
